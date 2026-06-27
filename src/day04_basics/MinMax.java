673fc6ac20649cf064519f34e871dcc40c469265
Debashish Pradhan <debashish.pradhan@gmail.com>
2026-06-27 21:31:13 +0530

    Updated the logic to lines of code

---
+++ src/day04_basics/MinMax.java
@@ -0,0 +1,36 @@
+package day04_basics;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MinMax {
+    public static void main(String[] args) {
+
+        System.out.print("How many numbers do you want to compare: ");
+        Scanner sc = new Scanner(System.in);
+        int countNum = sc.nextInt();//stores the number of integers provide by user
+        int[] myNumber = new int[countNum]; // array of integers provided by user
+
+        int i = 0;
+        while (i < countNum) {
+            System.out.print("Enter the number: ");
+            myNumber[i] = sc.nextInt();
+            i++;
+        }
+
+        int greatestNum = myNumber[0];
+
+        for (int k = 1; k < countNum; k++) {//assuming myNumber[0] is greatest number
+            if (myNumber[k] > greatestNum) {
+
+                greatestNum = myNumber[k];
+
+            } //else greatestNum = myNumber[0];
+        }
+
+        System.out.print("greatest num = " + greatestNum);
+    }
+}
+
+
+
