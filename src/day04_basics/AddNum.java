569d743bf3cc1da14778cd25b3b0c3331c8fdc58
Debashish Pradhan <debashish.pradhan@gmail.com>
2026-06-28 00:22:57 +0530

    Updated the logic to reduce lines of code

---
+++ src/day04_basics/AddNum.java
@@ -0,0 +1,17 @@
+package day04_basics;
+
+public class AddNum {//this class file adds two numbers. Those numbers are fed from the main method.
+
+    public static int addition(int a, int b) {//define the 'addition' method
+
+        return a + b;
+    }
+
+
+    public static void main(String[] args) {
+
+        int finalNum = addition(24, 43);
+
+        System.out.println("The final result is: " + finalNum);
+    }
+}
---
+++ src/day04_basics/AddNumUser.java
@@ -0,0 +1,24 @@
+package day04_basics;
+
+import java.util.Scanner;
+
+public class AddNumUser {
+
+    public static int addition(int a, int b){
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc1 = new Scanner(System.in);
+        System.out.print("Enter first number a: ");
+        int a = sc1.nextInt();
+
+        System.out.print("Enter second number b: ");
+        int b = sc1.nextInt();
+
+        int result = addition(a, b);
+
+        System.out.println("Result is " + result);
+    }
+}
